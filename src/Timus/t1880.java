a5c816e957fc5b41dd18c430f85f3c374cbddb33
P3RMA4 <as.surovnev@gmail.com>
2023-01-05 11:51:59 +0500

    Тимус

---
+++ src/Timus/t1880.java
@@ -0,0 +1,56 @@
+package Timus;
+import java.util.*;
+
+public class t1880 {
+
+    public static void main(String str[]) {
+        Scanner input = new Scanner(System.in);
+
+        final int n1 = input.nextInt();
+        int arr1[] = new int[n1];
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = input.nextInt();
+        }
+
+        final int n2 = input.nextInt();
+        int arr2[] = new int[n2];
+        for (int i = 0; i < n2; i++) {
+            arr2[i] = input.nextInt();
+        }
+
+        final int n3 = input.nextInt();
+        int arr3[] = new int[n3];
+        for (int i = 0; i < n3; i++) {
+            arr3[i] = input.nextInt();
+        }
+
+        Deque<Integer> match = new LinkedList<>();
+
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                if (arr1[i] < arr2[j])
+                    break;
+
+                if (arr1[i] == arr2[j]) {
+                    match.addLast(arr1[i]);
+                    break;
+                }
+            }
+        }
+
+        int result = 0;
+        for (Integer next : match) {
+
+            for (int j = 0; j < n3; j++) {
+                if (next < arr3[j])
+                    break;
+
+                if (next == arr3[j])
+                    result++;
+
+            }
+        }
+
+        System.out.println(result);
+    }
+}
---
+++ src/Timus/t1910.java
@@ -0,0 +1,29 @@
+package Timus;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class t1910 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(consoleReader.readLine());
+        int[] a = new int[n];
+        StringTokenizer tokenizer = new StringTokenizer(consoleReader.readLine(), " ");
+        consoleReader.close();
+
+        for (int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(tokenizer.nextToken());
+
+        int maxPower = 0;
+        int position = 0;
+        for (int i = 0; i < n - 2; i++) {
+            int sum = a[i] + a[i + 1] + a[i + 2];
+            if (sum > maxPower) {
+                maxPower = sum;
+                position = i + 2;
+            }
+        }
+
+        System.out.println(maxPower + " " + position);
+    }
+}
